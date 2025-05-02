8bc8be2db8ebfb3b27d319ede3020aec12b23ec8
mirmessier63 <jhn435897@gmail.com>
2025-05-02 16:49:14 +0300

    Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,11 @@ class PostsController < ApplicationController
       if Post.last.nil?
         @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: 0, user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: 1, category_id: params[:post][:category_id])
       else
-        @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: 0, user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: (Post.last.thread_id + 1), category_id: params[:post][:category_id])
+        @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: 0, user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: (Post.maximum("thread_id") + 1), category_id: params[:post][:category_id])
       end
       @post.save
     elsif params[:post][:type] == "comment"
-      @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: (Post.where(thread_id: params[:post][:thread_id]).last.comment_id + 1), user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: params[:post][:thread_id], category_id: params[:post][:category_id])
+      @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: (Post.where(thread_id: params[:post][:thread_id]).maximum("comment_id") + 1), user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: params[:post][:thread_id], category_id: params[:post][:category_id])
       @post.save
     end
     redirect_to request.referrer

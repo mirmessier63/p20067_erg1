099494db6cf59e299f6f2b8ddb5674564ecb3803
mirmessier63 <jhn435897@gmail.com>
2025-05-02 15:42:39 +0300

    Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,11 @@ class PostsController < ApplicationController
 
   def create
     if params[:post][:type] == "thread"
-      @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: 0, user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: (Post.last.thread_id + 1), category_id: params[:post][:category_id])
+      if == Post.last.thread_id.nil?
+        @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: 0, user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: 1, category_id: params[:post][:category_id])
+      else
+        @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: 0, user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: (Post.last.thread_id + 1), category_id: params[:post][:category_id])
+      end
       @post.save
     elsif params[:post][:type] == "comment"
       @post = Post.new(topic_id: params[:post][:topic_id], post_text: params[:post][:post_text], comment_id: (Post.where(thread_id: params[:post][:thread_id]).last.comment_id + 1), user_email: Current.user.email_address, text: params[:post][:text], created_at: DateTime.now, thread_id: params[:post][:thread_id], category_id: params[:post][:category_id])
